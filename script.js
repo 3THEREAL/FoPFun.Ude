a96693c69c599a9de953ede76a2dcafe04b76f90
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2020-06-17 16:19:28 +0600

    commit

--- script.js
+++ script.js
@@ -11,4 +11,37 @@ alert(a); */
 { BRACES }
 */
 
+/*
+Condition 
+= Command to insert value
+== 
+===
+!= Not Equal
 
+{} Code Block | Can be nested
+*/
+
+/*
+var a = 50;
+var b = 10;
+
+if (a<b) {
+    alert("Yes less than b");
+}
+if (a==b) {
+    alert("A is equal to b");
+}
+
+if ()
+*/
+
+var balance = 5000;
+
+if (balance >= 0) {
+    alert("The balance is positive");
+    if (balance > 10000) {
+        alert("The balance is too large");
+    }
+} else {
+    alert("The balance is negative");
+}
